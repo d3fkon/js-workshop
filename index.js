240279b6196582ea2c5c666a64982b2587fc93f9
d3fkon <d3fkonmusic@gmail.com>
2017-09-28 18:07:25 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Stage200
+// String concatination
+
+var str1 = 'Sample String'; // String declarations 
+
+var str2 = 'Lol';
+
+var str3 = str1 + str2; // str3 = 'Sample String Lol'
+
+var str4 = 'I can ' + 'concatinate with +';
+
+var str5 = 'Everything is ' + str2;
+
+var str6 = 'Hello' + ' Hai' + ' Good Morning'; 
+
+// Output
+
+console.log('str1 => ' + str1);
+console.log('str2 => ' + str2);
+console.log('str3 => ' + str3);
+console.log('str4 => ' + str4);
+console.log('str5 => ' + str5);
+console.log('str6 => ' + str6);
