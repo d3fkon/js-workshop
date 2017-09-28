e98037f5b132f11b34a4b0f468f5be43c348f6fc
d3fkon <d3fkonmusic@gmail.com>
2017-09-28 17:47:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Stage200
+// ES5
+
+// declaration 
+
+var variableOne;
+
+// --------------------------------------------------------------------------
+
+// initialization
+
+var variableTwo = "HELLO WORLD";
+
+// --------------------------------------------------------------------------
+
+// assignment
+
+variableOne = 1337;
+
+// --------------------------------------------------------------------------
+
+// console output
+
+console.log('variable 1 =>', variableOne, '\nvariable 2 =>', variableTwo);
