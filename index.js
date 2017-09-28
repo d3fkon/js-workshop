846e1e8770f364b88c10625b885a72b3f61a1951
d3fkon <d3fkonmusic@gmail.com>
2017-09-28 17:41:58 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Stage2
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Welcome!
+ * 
+ * We will be working with a git repository to walk through the different stages of learning nodejs and js
+ * 
+ * 
+ * Before doing anything! Just remember these three words
+ * 
+ * SAVE -> STASH -> CHECKOUT
+ * 
+ * I will soon explain what they mean and how to perform them,
+ * but just remember to perform these before moving to a new stage.
+ * 
+ * 
+ * 
+ * Enjoy the ride!
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
