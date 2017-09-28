2b18eec96816f96434366e00b993d744619e889d
d3fkon <d3fkonmusic@gmail.com>
2017-09-28 17:56:07 +0530

    initial commit

--- index.js
+++ index.js
@@ -21,3 +21,28 @@ variableOne = 1337;
 // console output
 
 console.log('variable 1 =>', variableOne, '\nvariable 2 =>', variableTwo);
+
+// if-else, for, while, do while, switch all work he same as 'C'
+
+var x = 2;
+var y = 1;
+if (x > y) {
+    console.log(x + ' is greater than ' + y);
+}
+else {
+    console.log(y + ' is greater than' + x);    
+}
+// --------------------------------------------------------------------------
+
+var i;
+for (i=0; i<5; i++) {
+    console.log('i => '+ i);
+}
+
+// --------------------------------------------------------------------------
+
+while(x>=y) {
+    console.log(x + ' is greater than ' +  y + ' but not for long!');
+    y++;
+}
+
