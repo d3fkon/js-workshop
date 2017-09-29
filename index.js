7d93bcee01c9fca292086b5413d842cf1cdad320
d3fkon <d3fkonmusic@gmail.com>
2017-09-29 16:52:56 +0530

    added format specifiers

--- index.js
+++ index.js
@@ -2,37 +2,27 @@
 
 // declaration 
 var variableOne;
-// --------------------------------------------------------------------------
+
 // initialization
 var variableTwo = "HELLO WORLD";
-// --------------------------------------------------------------------------
+
 // assignment
 variableOne = 1337;
-// --------------------------------------------------------------------------
+
 // console output (scanf)
-console.log('variable 1 => ' +  variableOne + '\nvariable 2 => ' + variableTwo);
-// --------------------------------------------------------------------------
+console.log('variable 1 => %s \nvariable 2 => %d', variableOne, variableTwo);
 
 // if-else, for, while, do while, switch all work he same as 'C'
 var x = 2;
 var y = 1;
 if (x > y) {
-    console.log(x + ' is greater than ' + y);
+    console.log('%d is greater than %d', x, y);
 }
 else {
     console.log(y + ' is greater than' + x);    
 }
-// --------------------------------------------------------------------------
 
 var i;
 for (i=0; i<5; i++) {
-    console.log('i => '+ i);
+    console.log('i => %d', i);
 }
-
-// --------------------------------------------------------------------------
-
-while(x>=y) {
-    console.log(x + ' is greater than ' +  y + ' but not for long!');
-    y++;
-}
-
