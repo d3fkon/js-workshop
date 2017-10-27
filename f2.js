82c849a711239a9fb22a77ebcdd8cc5c77ea1f3b
d3fkon <d3fkonmusic@gmail.com>
2017-10-27 19:40:03 +0530

    initial commit

---
+++ f2.js
@@ -0,0 +1,7 @@
+// module.exports object is used when a file has to export something to another file, which requires it
+
+var toBeExported = "I come from f2.js";
+// Syntax
+// module.exports = _variableName
+
+module.exports = toBeExported;
