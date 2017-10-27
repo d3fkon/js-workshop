f706ff1375294fe249eea3f69f59fd974b69409c
d3fkon <d3fkonmusic@gmail.com>
2017-10-27 19:30:38 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-// Functions as variables
+// Handling files
 
-var fun1 = function (parameters) {
-    var value = parameters;
-    return value;
-}
+var importedStuff = require('./f2');
 
-var returnedValue = fun1("Arguments");
-
-// console.log(returnedValue);
-
-// Passing functions as parameters to other functions
-
-var fun1 = function (param) {
-    console.log("This is fun1. And you passed: " + param);
-}
-
-var fun2 = function (param) {
-    console.log("This is fun2. And you passed: " + param);
-}
-
-var mainFun = function (f1, f2) {
-    f1("apples");
-    f2("oranges");
-}
-
-mainFun(fun1, fun2);
+console.log(importedStuff);
