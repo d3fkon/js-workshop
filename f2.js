7d9f59943bab2d6507bab4fc47ac2a65180fd438
d3fkon <d3fkonmusic@gmail.com>
2017-10-27 20:33:49 +0530

    stage-10

--- f2.js
+++
@@ -1,7 +0,0 @@
-// We can also export functions as variables to be called in other files
-
-var functionToBeExported = function (args) {
-    // log all the arguments passed to the function
-    console.log(args);
-}
-module.exports = functionToBeExported;
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var importedFunction = require('./f2');
+// 1. Create two files named f1.js and Float32Array.js using the 'touch' command.
+// 2. Send the object below to the function imported from f1.js which displays the fruits
+// 3. Also, send the same object to the function imported from f2.js which displays the vegetables
+// 4. Both functions in f1 and f2 should console.log(obj.fruits) and console.log(obj.vegetables) respectively.
 
-importedFunction('Passing a parameter from index.js')
+var obj = {
+    fruits: ["apples", "oranges", "grapes"],
+    vegetables: ["onions", "tomatos"]
+}
+
+// var f1 = require(.......)
+// f1(obj)
+// same for f2
