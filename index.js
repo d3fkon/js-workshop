b242f0abad5b2b96f67c42d406288d3f332fcb9e
d3fkon <d3fkonmusic@gmail.com>
2017-10-28 07:17:23 +0530

    fixed typos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1. Create two files named f1.js and Float32Array.js using the 'touch' command.
+// 1. Create two files named f1.js and f2.js using the 'touch' command.
 // 2. Send the object below to the function imported from f1.js which displays the fruits
 // 3. Also, send the same object to the function imported from f2.js which displays the vegetables
 // 4. Both functions in f1 and f2 should console.log(obj.fruits) and console.log(obj.vegetables) respectively.
